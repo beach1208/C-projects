831f7fc98ce0d532615d5c1b43f5276b8b612f82
marlavidal <35268490+marlavidal@users.noreply.github.com>
2018-08-21 10:22:44 -0700

    the split function. the 11 of part 2.

---
+++ vc_strsplit.c
@@ -0,0 +1,38 @@
+/* ************************************ */
+/*                                      */
+/* vc_strsplit.c                        */
+/*                                      */
+/* By: Marla                            */
+/*                                      */
+/* ************************************ */
+#include <stdlib.h>
+#include <unistd.h>
+
+char **vc_strsplit(char const *s, char c)
+{
+    size_t start;
+	size_t end;
+	size_t i;
+	size_t words;
+	char **tab;
+
+	words = 0;
+	tab = NULL;
+	i = 0;
+	if (s && ((tab = (char **)malloc(sizeof(*tab) * (vc_strlen(s) / 2 + 2)))))
+	{
+		while (i < vc_strlen(s))
+		{
+			while (s[i] == c && s[i])
+				i++;
+			start = i;
+			while (s[i] != c && s[i])
+				i++;
+			end = i;
+			if ((end - start) > 0)
+				tab[words++] = vc_strsub(s, start, end - start);
+		}
+		tab[words] = 0;
+	}
+	return (tab);
+}
