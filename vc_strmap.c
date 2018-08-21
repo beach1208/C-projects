7fb31bd431af6f986bcee8033f609abce49e9b2c
marlavidal <35268490+marlavidal@users.noreply.github.com>
2018-08-21 16:04:38 -0700

    The correct function. I just don't if can stay both of the function in the same file.

--- vc_strmap.c
+++ vc_strmap.c
@@ -1,22 +1,35 @@
+/* ************************************ */
+/*                                      */
+/* vc_strmap.c                          */
+/*                                      */
+/* By: Kenta, Marla                     */
+/*                                      */
+/* ************************************ */
+
 #include "libvc.h"
+
+int vc_strlen(char *str)
+{
+  int counter = 0;
+  while (*str)
+  {
+    counter++;
+    str++;
+  }
+  return counter;
+}
 
 char *vc_strmap(char const *s, void (*f)(char))
 {
-	int	i;
-	char *str;
+  int i;
+  int len = vc_strlen((char *)s);
+  char *str = ((char *)malloc((len + 1) * sizeof(char)));
+  *str = *s;
 
-	i = 0;
-	if (s == '\0' || f == '\0')
-		return ('\0');
-	str = (char *)malloc((vc_strlen(s) + 1) * sizeof(char));
-	if (str)
-	{
-		while (s[i] != '\0')
-		{
-			str[i] = (*f)(s[i]);
-			i++;
-		}
-		str[i] = '\0';
-	}
-	return (str);
+  for (i = 0; i < len; i++)
+  {
+    (*f)(s[i]);
+  }
+  str[i] = '\0';
+  return str;
 }
