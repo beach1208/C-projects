bd10aa0755825b9682d86e9f66b800e860973c12
Kenta Kodashima <kentakodashima@gmail.com>
2018-08-23 14:54:08 -0700

    Modified the implementation of vc_strtrim.c

--- vc_strtrim.c
+++ vc_strtrim.c
@@ -22,20 +22,51 @@ static int length(char const *str)
   return counter;
 }
 
+static int beginingSpaceCount(char const *s)
+{
+  int counter = 0;
+  int i = 0;
+  while (*s)
+  {
+    if (s[i] != ' ' && s[i] != '\t' && s[i] != '\n')
+    {
+      break;
+    }
+    counter++;
+    i++;
+  }
+  return counter;
+}
+
+static int endSpaceCount(char const *s)
+{
+  int counter = 0;
+  int i = length(s);
+  while (*s)
+  {
+    if (s[i] != ' ' && s[i] != '\t' && s[i] != '\n')
+    {
+      break;
+    }
+    counter++;
+    i--;
+  }
+  return counter;
+}
+
 char *vc_strtrim(char const *s)
 {
   int sLen = length(s);
   char *newS = malloc(sizeof sLen);
+  int beginingSpaceNum = beginingSpaceCount(s);
+  int endSpaceNum = endSpaceCount(s);
   int i;
   int j = 0;
 
-  for (i = 0; i < sLen; i++)
+  for (i = beginingSpaceNum; i < sLen - endSpaceNum; i++)
   {
-    if (s[i] != ' ' && s[i] != '\t' && s[i] != '\n')
-    {
-      newS[j] = s[i];
-      j++;
-    }
+    newS[j] = s[i];
+    j++;
   }
   newS[j] = '\0';
 
